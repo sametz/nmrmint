72143bb848b48972aeaf913c0d74484d33e9860c
Geoffrey M. Sametz <sametz@udel.edu>
2016-04-09 16:42:12 -0400

    Hamiltonian optimized! by using arrays instead of sparse matrices. nmrmath.py now has optimized hamiltonian, transition_matrix and simsignals functions. nspin should be refactored in future to return dense array of coupling constants, not sparse.

--- ReichDNMR/nmrmath.py
+++ ReichDNMR/nmrmath.py
@@ -1,41 +1,14 @@
-# coding: utf-8
 """
-Adding notes on speeding code up:
--note the error that pops up when calculating the transition matrix. "Changing
-the sparsity structure of a csc_matrix is expensive. lil_matrix is more
-efficient."
--try using csc_matrix as much as possible
--biggest time sink right now is creating the spectrum list. Maybe replace all
-use of python lists with numpy/scipy array
--code runs faster in functions than in global scope (faster with local variables
-than global)
--avoid dots. e.g. from module import name
--avoid loops. "vector operations on ndarrays are much faster than Python loops"
--boolean mask? e.g. x[(x > 0.01)] = 0 instead of for/if
--"use array slicing, masks, fancy indexing, and broadcasting to eliminate loops"
--numpy bitwise_xor(a,b)
--append(a,b) or something else to add to array
+This version of nmrmath features speed-optimized hamiltonian, simsignals,
+and transition_matrix functions. Up to at least 8 spins, the new non-sparse
+Hamilton code is about 10x faster. The overall performance is dramatically
+better than the original code.
 """
 
 import numpy as np
 
 from scipy.linalg import eigh
-from scipy.sparse import kron, csc_matrix, lil_matrix
-
-
-def nlist(length):
-    """
-    creates a list (length) long of empty lists.
-    This is probably redundant with a built-in python/numpy/scipy function,
-    so consider replacing in future.
-    Input:
-        :param length: number of empty lists in list
-    Returns:
-        a list of [] x length
-
-    """
-    # noinspection PyUnusedLocal
-    return [[] for l in range(length)]
+from scipy.sparse import kron, csc_matrix, csr_matrix, lil_matrix, bmat
 
 
 def popcount(n=0):
@@ -64,7 +37,6 @@ def is_allowed(m=0, n=0):
     return popcount(m ^ n) == 1
 
 
-# noinspection PyPep8Naming
 def transition_matrix(n):
     """
     Creates a matrix of allowed transitions.
@@ -78,13 +50,15 @@ def transition_matrix(n):
 
     :returns: a transition matrix that can be used to compute the intensity of
     allowed transitions.
-
     """
-    T = csc_matrix((n, n))  # sparse matrix created
-    for i in range(n):
-        for j in range(n):
+    # function was optimized by only calculating upper triangle and then adding
+    # the lower.
+    T = lil_matrix((n, n))  # sparse matrix created
+    for i in range(n-1):
+        for j in range(i+1, n):
             if is_allowed(i, j):
                 T[i, j] = 1
+    T = T + T.T
     return T
 
 
@@ -94,120 +68,113 @@ def hamiltonian(freqlist, couplings):
     Computes the spin Hamiltonian for spin-1/2 nuclei.
     inputs for n nuclei:
         :param freqlist: a list of frequencies in Hz of length n
-        :param couplings: a sparse n x n matrix of coupling constants in Hz
-    Returns: a sparse Hamiltonian matrix
+        :param couplings: an n x n array of coupling constants in Hz
+    Returns: a Hamiltonian array
     """
     nspins = len(freqlist)
-    print('Defining unit matrices')
+
     # Define Pauli matrices
-    # change below back to csr if no improvement
-    sigma_x = csc_matrix(np.matrix([[0, 1/2], [1/2, 0]]))
-    sigma_y = csc_matrix(np.matrix([[0, -1j/2], [1j/2, 0]]))
-    sigma_z = csc_matrix(np.matrix([[1/2, 0], [0, -1/2]]))
-    unit = csc_matrix(np.matrix([[1, 0], [0, 1]]))
-    print('Unit matrices defined')
+    sigma_x = np.matrix([[0, 1/2], [1/2, 0]])
+    sigma_y = np.matrix([[0, -1j/2], [1j/2, 0]])
+    sigma_z = np.matrix([[1/2, 0], [0, -1/2]])
+    unit = np.matrix([[1, 0], [0, 1]])
 
-    print('Generating lists of Lx/y/z matrices')
-    # The following empty lists will be used to store the
-    # //insert description here
-
-    Lx = nlist(nspins)
-    Ly = nlist(nspins)
-    Lz = nlist(nspins)
+    # The following empty arrays will be used to store the
+    # Cartesian spin operators.
+    Lx = np.empty((1, nspins), dtype='object')
+    Ly = np.empty((1, nspins), dtype='object')
+    Lz = np.empty((1, nspins), dtype='object')
 
     for n in range(nspins):
-        Lx_current = 1; Ly_current = 1; Lz_current = 1
+        Lx[0, n] = 1
+        Ly[0, n] = 1
+        Lz[0, n] = 1
         for k in range(nspins):
-            # Need to use scipy kron, not np.kron with sparse matrices
-            if k == n:  # Diagonal element
+            if k == n:                                  # Diagonal element
+                Lx[0, n] = np.kron(Lx[0, n], sigma_x)
+                Ly[0, n] = np.kron(Ly[0, n], sigma_y)
+                Lz[0, n] = np.kron(Lz[0, n], sigma_z)
+            else:                                       # Off-diagonal element
+                Lx[0, n] = np.kron(Lx[0, n], unit)
+                Ly[0, n] = np.kron(Ly[0, n], unit)
+                Lz[0, n] = np.kron(Lz[0, n], unit)
 
-                Lx_current = kron(Lx_current, sigma_x)
-                Ly_current = kron(Ly_current, sigma_y)
-                Lz_current = kron(Lz_current, sigma_z)
-            else:
-                Lx_current = kron(Lx_current, unit)
-                Ly_current = kron(Ly_current, unit)
-                Lz_current = kron(Lz_current, unit)
+    Lcol = np.vstack((Lx, Ly, Lz)).real
+    Lrow = Lcol.T  # As opposed to sparse version of code, this works!
+    Lproduct = np.dot(Lrow, Lcol)
 
-        Lx[n] = Lx_current
-        Ly[n] = Ly_current
-        Lz[n] = Lz_current
-
-    print('Lx/y/z matrices compiled')
-    print('Calculating Hamiltonian')
     # Hamiltonian operator
-    H = csc_matrix((2**nspins, 2**nspins))
+    H = np.zeros((2**nspins, 2**nspins))
 
-    # Zeeman interactions:
+    # Add Zeeman interactions:
     for n in range(nspins):
-        H = H + freqlist[n] * Lz[n]
-    print('Diagonal elements computed')
+        H = H + freqlist[n] * Lz[0, n]
+
     # Scalar couplings
 
     # Testing with MATLAB discovered J must be /2.
-    # Believe it is related to the fact that in the simulation freqs are *2pi,
-    # but Js by pi only. Video is supposed to explain why.
-
+    # Believe it is related to the fact that in the SpinDynamics.org simulation
+    # freqs are *2pi, but Js by pi only. Video is supposed to explain why.
+    scalars = 0.5 * couplings
+    scalars = np.multiply(scalars, Lproduct)
     for n in range(nspins):
         for k in range(nspins):
-            if n != k:
-                # noinspection PyTypeChecker
-                H += (couplings[n, k] / 2) * (Lx[n] * Lx[k] +
-                                              Ly[n] * Ly[k] +
-                                              Lz[n] * Lz[k])
-    print('Hamiltonian computed')
+            H += scalars[n, k].real
+
     return H
 
 
-# noinspection PyPep8Naming
+# noinspection PyPep8Naming,PyShadowingNames
 def simsignals(H, nspins):
     """
     Solves the spin Hamiltonian H and returns a list of (frequency, intensity)
     tuples. Nuclei must be spin-1/2.
     Inputs:
-        :param H: a sparse spin Hamiltonian
+        :param H: a Hamiltonian array
         :param nspins: number of nuclei
-    Returns:
-        peaklist: a list of (frequency, intensity) tuples.
-
-
+    :return spectrum: a list of (frequency, intensity) tuples.
     """
-    print('Calculating eigensystem')
+    # This routine was optimized for speed by vectorizing the intensity
+    # calculations, replacing a nested-for signal-by-signal calculation.
+    # Considering that hamiltonian was dramatically faster when refactored to
+    # use arrays instead of sparse matrices, consider an array refactor to this
+    # function as well.
+
+    # The eigensolution calculation apparently must be done on a dense matrix,
+    # because eig functions on sparse matrices can't return all answers?!
     # Using eigh so that answers have only real components and no residual small
-    # j components b/c of rounding errors
+    # unreal components b/c of rounding errors
+    E, V = np.linalg.eigh(H)    # V will be eigenvectors, v will be frequencies
 
-    E, V = eigh(H.todense())  # V will be eigenvectors, v will be frequencies
-    print('Eigensystem solved; converting eigenvectors to sparse')
+    # Eigh still leaves residual 0j terms, so:
     V = np.asmatrix(V.real)
-    V = csc_matrix(V)         # Consider refactoring if this is confusing
-    print('V converted to csc matrix.')
 
-    print('Calculating the transition matrix')
-    T = transition_matrix(2**nspins)
-    print('Transition matrix calculated')
+    # Calculate signal intensities
+    Vcol = csc_matrix(V)
+    Vrow = csr_matrix(Vcol.T)
+    m = 2 ** nspins
+    T = transition_matrix(m)
+    I = Vrow * T * Vcol
+    I = np.square(I.todense())
 
-    print('Collecting spectrum')
     spectrum = []
-    for i in range(2**nspins):
-        for j in range(i, 2**nspins):
-            if j != i:
-                intensity = (V[:, i].T * T * V[:, j])[0, 0]**2
-                # apparently returns 2D matrix
-                # consider refactor to float
-                if intensity > 0.01:
-                    v = abs(E[i] - E[j])
-                    spectrum.append((v, intensity))
-    print('Spectrum obtained.')
+    for i in range(m-1):
+        for j in range(i+1, m):
+            if I[i, j] > 0.01:  # consider making this minimum intensity
+                                # cutoff a function arg, for flexibility
+                v = abs(E[i] - E[j])
+                spectrum.append((v, I[i, j]))
+
     return spectrum
 
 
-# noinspection PyUnreachableCode,PyPep8Naming
+# noinspection PyUnreachableCode,PyPep8Naming,PyShadowingNames
 def nspinspec(freqs, couplings):
     """
     Function that calculates a spectrum for n spin-half nuclei.
     Inputs:
         :param freqs: a list of n nuclei frequencies in Hz
-        :param couplings: an n x n sparse matrix of couplings in Hz. The order
+        :param couplings: an n x n array of couplings in Hz. The order
         of nuclei in the list corresponds to the column and row order in the
         matrix, e.g. couplings[0][1] and [1]0] are the J coupling between
         the nuclei of freqs[0] and freqs [1].
@@ -218,17 +185,12 @@ def nspinspec(freqs, couplings):
 
 
 if __name__ == '__main__':
-    # Data from the Frank Rioux 3-spin simulation
-    zeeman_freqs = np.array([430, 265, 300])
-    print('Frequencies: ', zeeman_freqs)
-    n = len(zeeman_freqs)
-    scalar_couplings = lil_matrix((n, n))
-    # J01 = 15, J02 = 11, J12 = 3
-    scalar_couplings[0, 1] = 7
-    scalar_couplings[0, 2] = 15
-    scalar_couplings[1, 2] = 1.5
-    scalar_couplings = scalar_couplings + scalar_couplings.T
-    print('Scalar couplings:')
-    print(scalar_couplings.todense())
+    from nspin import reich_list
+    from nmrplot import nmrplot as nmrplt
 
-    print(nspinspec(zeeman_freqs, scalar_couplings))
+    test_freqs, test_couplings = reich_list()[8]
+
+    # refactor reich_list to do this!
+    test_couplings = test_couplings.todense()
+    spectrum = nspinspec(test_frequencies, test_couplings)
+    #nmrplt(nspinspec(test_freqs, test_couplings), y=24)
